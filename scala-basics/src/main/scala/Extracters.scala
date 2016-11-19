f3ab014a798bd1713d4486f79a3127ea0a14b8ce
Govind <bhonegovind@gmail.com>
2016-11-19 23:38:36 +0530

    Generics ,Extracters ,Sequence Comprehension,Variance

---
+++ scala-basics/src/main/scala/Extracters.scala
@@ -0,0 +1,121 @@
+/**
+  * Created by govind.bhone on 11/19/2016.
+  */
+object Extracters extends App {
+
+  object Twice {
+    def apply(x: Int): Int = x * 2
+
+    def unapply(z: Int): Option[Int] = if (z % 2 == 0) Some(z / 2) else None
+  }
+
+  object TwiceTest extends App {
+    val x = Twice(21)
+    x match {
+      case Twice(n) => Console.println(n)
+    } // prints 21
+  }
+
+  trait User {
+    def name: String
+  }
+
+  class PrepaidUser(val name: String) extends User
+
+  class PostPaidUser(val name: String) extends User
+
+  object PrepaidUser {
+    def apply(name: String) = new PrepaidUser(name)
+
+    def unapply(user: PrepaidUser): Option[String] = Some(user.name)
+  }
+
+  object PostPaidUser {
+    def apply(name: String) = new PostPaidUser(name)
+
+    def unapply(user: PostPaidUser): Option[String] = Some(user.name)
+  }
+
+  val prepaidUser: User = PrepaidUser("govind")
+
+  prepaidUser match {
+    case PrepaidUser(user) => println(user)
+    case PostPaidUser(user) => println(user)
+  }
+
+  println(PrepaidUser.unapply(new PrepaidUser("govind")))
+
+
+  trait ScoreUser {
+    def name: String
+
+    def score: Int
+  }
+
+  class ScoreLessUser(val name: String, val score: Int, val upgradeProbability: Double)
+    extends ScoreUser
+
+  class ScoreHighUser(val name: String, val score: Int) extends ScoreUser
+
+  object ScoreLessUser {
+    def unapply(user: ScoreLessUser): Option[(String, Int, Double)] =
+      Some((user.name, user.score, user.upgradeProbability))
+  }
+
+  object ScoreHighUser {
+    def unapply(user: ScoreHighUser): Option[(String, Int)] = Some((user.name, user.score))
+  }
+
+  val user: ScoreUser = new ScoreLessUser("govind", 300, 0.1d)
+  user match {
+    case ScoreLessUser(name, _, p) =>
+      if (p > 0.01) name + ", what can we do for you today?" else "Hello " + name
+    case ScoreHighUser(name, _) => "Welcome back, dear " + name
+  }
+
+
+  //===================Boolean extracters
+
+  val scoreUser: ScoreUser = new ScoreLessUser("govind", 300, 0.1d)
+
+  object BooleanExtracters {
+    def unapply(scoreUser: ScoreUser): Boolean = {
+      if (scoreUser.score > 0.01) true else false
+    }
+  }
+
+  scoreUser match {
+    case freeUser@BooleanExtracters() => println(freeUser.name)
+    case _ =>
+  }
+
+  val xs = 58 #:: 43 #:: 93 #:: Stream.empty
+  val res = xs match {
+    case first #:: second #:: _ => first - second
+    case _ => -1
+  }
+
+  println(res)
+
+  object #:: {
+    def unapply[A](xs: Stream[A]): Option[(A, Stream[A])] =
+      if (xs.isEmpty) None
+      else Some((xs.head, xs.tail))
+  }
+
+  val xs1 = 58 #:: 43 #:: 93 #:: Stream.empty
+  xs1 match {
+    /*    case head #:: tail => // Infix Notation
+          println(head)
+          println(tail.toList)*/
+
+    //case first #:: second #:: third=>
+    case #::(first, #::(second, _)) =>
+      println(first)
+      println(second)
+      first - second
+    case _ => -1
+  }
+
+
+}
